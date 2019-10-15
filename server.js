3bb94a87e13506c5ff0a819673beb7705a1c7d82
Apirat Chiwakanit <apirat.c@kbtg.tech>
2019-10-15 09:25:07 +0700

    event emit socket io

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const server = require('http').createServer()
 const io = require('socket.io')(server)
 
+io.on('connection', socket => {
+    socket.on('emit', () => {
+        console.log('receive data')
+    })
+})
+
 server.listen(8080, err => {
     if (err) throw err
     console.log('server is listening to port 8080')
